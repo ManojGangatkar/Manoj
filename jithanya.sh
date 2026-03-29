64c5a2cb62315bf6924e63d6dde76cb4aac20063
Ubuntu <ubuntu@ip-172-31-40-81.ap-south-1.compute.internal>
2026-03-29 06:52:35 +0000

    This is the output of Node health

---
+++ jithanya.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#Author: Manoj kumar
+#Date: 29-03-2026
+#
+#This script outputs the node health
+#
+#Version: v1
+#
+#
+##########################
+#
+ set -x
+ df -h
+ free -g
+ nproc
+ 
+
